b2ef3036e54a422df38c062b11784540e00573ff
Shivom <shivompaudel@icloud.com>
2022-12-14 16:49:20 -0600

    started services

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nav/>
       <Landing/>
       <About/>
+      <Services/>
     </div>
   );
 }
